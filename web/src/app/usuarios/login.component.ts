467bedf241fd4c1548a6b36383c0e009a4f82009
jose <jose.jofre84@gmail.com>
2018-12-21 18:00:36 -0300

    commit angular web

---
+++ web/src/app/usuarios/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent implements OnInit {
+
+  titulo:string = 'Iniciar Sesión';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
